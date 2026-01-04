d4f978742b805a19d7f003adb890d35b849def48
hemantjaipur <hemantjhalani29@gmail.com>
2026-01-04 12:11:59 +0530

    Create doctr.py
    Added Doctr for extracting text from Image

---
+++ doctr.py
@@ -0,0 +1,39 @@
+import os
+os.environ['USE_TORCH'] ='1'
+from doctr.io import DocumentFile
+from doctr.models import ocr_predictor
+
+# 1. Load a pre-trained OCR model
+# Use the ocr_predictor factory function with desired architectures
+model = ocr_predictor(pretrained=True,assume_straight_pages=False,
+    export_as_straight_boxes=False)
+
+# 2. Load your document (image or PDF)
+# You can use DocumentFile.from_images or DocumentFile.from_pdf
+doc = DocumentFile.from_images(["/content/site_plan_2.png"])
+
+# 3. Perform the OCR
+result = model(doc)
+
+# 4. Export the results to a structured format (e.g., JSON)
+json_output = result.export()
+
+# Accessing the orientation for each page
+for page in result.pages:
+    # The orientation attribute gives the angle in degrees
+    orientation_angle = page.orientation
+    print(f"Page orientation: {orientation_angle} degrees")
+
+age = result.pages[0]
+img_height, img_width = page.dimensions
+
+# 5. Print the extracted text (simple iteration)
+
+
+for page in json_output['pages']:
+    for block in page['blocks']:
+        for line in block['lines']:
+            line_text = " ".join([word['value'] for word in line['words']])
+            print(line_text)
+
+
